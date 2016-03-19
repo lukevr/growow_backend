6f8b03a6e4c6a9fb9ca241eac0430cc67de67beb
Vsevolod Kolchinsky <sugarcoated@d.kiev.ua>
2016-03-19 08:24:05 +0200

    zmq logic implemented

--- API/apiwebapp/core/views.py
+++ API/apiwebapp/core/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import View
-
+import zmq
 import logging
 log = logging.getLogger(__name__)
 
@@ -14,6 +14,14 @@ class JSONResponseMixin(object):
 class IntentVideo(JSONResponseMixin,View):
     def get(self,request):
         # LET'S HAVE LOGIC HERE
+        
+        zmq_ctx = zmq.Context()
+        publisher = zmq_ctx.socket(zmq.PUB)
+        publisher.bind("tcp://*:5563")
+        publisher.send_multipart([b"P", b"Play Video"])
+        publisher.close()
+        zmq_ctx.term()
+        
         return self.render_to_response({
                                         'result':'OK',
                                         })    
@@ -22,6 +30,14 @@ class IntentVideo(JSONResponseMixin,View):
 class StopVideo(JSONResponseMixin,View):
     def get(self,request):
         # LET'S HAVE LOGIC HERE
+
+        zmq_ctx = zmq.Context()
+        publisher = zmq_ctx.socket(zmq.PUB)
+        publisher.bind("tcp://*:5563")
+        publisher.send_multipart([b"S", b"Stop Video"])
+        publisher.close()
+        zmq_ctx.term()
+
         return self.render_to_response({
                                         'result':'OK',
                                         })    
