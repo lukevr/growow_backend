acfa24255d8dcec097c5f7a88b1f4c94f9b74ba6
Vsevolod Kolchinsky <sugarcoated@d.kiev.ua>
2016-03-19 07:48:26 +0200

    endpoint enabled

--- API/apiwebapp/apiwebapp/urls.py
+++ API/apiwebapp/apiwebapp/urls.py
@@ -14,7 +14,12 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from core import views
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^api/intent_video', views.IntentVideo.as_view(),
+        name='intent_video'),
+
 ]
--- API/apiwebapp/core/views.py
+++ API/apiwebapp/core/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.views.generic import View
 
-# Create your views here.
+import logging
+log = logging.getLogger(__name__)
+
+from django.http import JsonResponse
+
+class JSONResponseMixin(object):
+    def render_to_response(self, context, **response_kwargs):
+        return JsonResponse(self.get_data(context), **response_kwargs)
+
+
+class IntentVideo(JSONResponseMixin,View):
+    def get_data(self,ctx):
+        log.debug(repr(ctx))
+        return ctx
+    
+    def get(self,request):
+        log.debug(repr(request))
+        pass
+    
+    
+    
+
